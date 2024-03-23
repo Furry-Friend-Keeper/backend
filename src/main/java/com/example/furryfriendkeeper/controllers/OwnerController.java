8e355430af0a3ec1c183c133451d955ee67cb83e
Denphum Nakglam <denphum.nakglam@mail.kmutt.ac.th>
2024-03-23 23:36:41 +0700

    [Update] annotation

--- src/main/java/com/example/furryfriendkeeper/controllers/OwnerController.java
+++ src/main/java/com/example/furryfriendkeeper/controllers/OwnerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
