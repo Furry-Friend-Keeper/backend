8650dd36448171ae2934a17c1a24acf2daf32312
Thanakrit Paithun <Thanakit.7653@gmail.com>
2024-04-27 19:10:08 +0700

    backend_notification_update

--- src/main/java/com/example/furryfriendkeeper/repositories/PetownerNotificationRepository.java
+++ src/main/java/com/example/furryfriendkeeper/repositories/PetownerNotificationRepository.java
@@ -3,9 +3,19 @@ package com.example.furryfriendkeeper.repositories;
 import com.example.furryfriendkeeper.entities.Petkeepernotification;
 import com.example.furryfriendkeeper.entities.Petownernotification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface PetownerNotificationRepository extends JpaRepository<Petownernotification,Integer> {
+    @Query(value = "Update petownernotification p set ReadStatus = 1 where p.PetOwnerNotiId = :notiId",nativeQuery = true)
+    void updatePetownerRead(@Param("notiId")Integer notiId);
+
+    @Query(value = "Select * from petownernotification p where p.PetOwnerId = :ownerId",nativeQuery = true)
+    List<Petownernotification> getAllNotiByOwnerId(@Param("ownerId")Integer ownerId);
+
 }
